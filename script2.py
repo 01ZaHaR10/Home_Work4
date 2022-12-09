78a8e51b9034fa703f739870fca0ee75b5f17681
Zahar Falyhov <zahar.falyhov@list.ru>
2022-12-09 16:37:38 +0300

    Script2

--- script2.py
+++ script2.py
@@ -1,4 +1,5 @@
 from PIL import ImageColor
 colors_list = list(ImageColor.colormap.keys())
 colors_dict = dict((colors_list[i], ImageColor.getcolor(colors_list[i], "RGB" )) for i in range(len(colors_list)))
-print(colors_dict)
+for i in colors_dict.keys():
+    print(f"Color - {i} : RGB - {colors_dict[i]}")
